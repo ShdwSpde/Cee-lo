f75153c04727df2645867303b521e90094761598
Mike Spade <mikespadeny@gmail.com>
2022-03-16 10:46:58 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+let userDice1
+let userDice2 
+let userDice3
+let computerDice1
+let computerDice2
+let computerDice3
+
+
+
+function Ace
+if userDice1 === userDice2 === userDice3 
+console.log('Head Cracckkk, scoop that cash')
+payout
+
+function Roll
+let userDice1 = Math.floor(Math.random() * 6) + 1
+let userDice2 = Math.floor(Math.random() * 6) + 1
+let userDice3 = Math.floor(Math.random() * 6) + 1
+console.log(userDice1)
+console.log(userDice2)
+console.log(userDice3)
+
+
+
+function goodUserRoll
+if userDice1 === userDice2
+if userDice2 === userDice3
+if userDice3 === userDice1
+
+
+function rollUserDice
+
+function payout
+
+
+
